c1a82680651c4023157c773d1dcea5d9658266a7
Сергей «keksVlz» <>
2022-09-07 05:20:41 +0000

    Initial commit

---
+++ replit.nix
@@ -0,0 +1,11 @@
+{ pkgs }: {
+  deps = [
+    pkgs.libosmscout
+    pkgs.imagemagick6_light
+    pkgs.python38Full
+  ];
+  env = {
+    PYTHONBIN = "${pkgs.python38Full}/bin/python3.8";
+    LANG = "en_US.UTF-8";
+  };
+}
